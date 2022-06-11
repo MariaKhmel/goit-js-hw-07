994dac260477f810a40266e61c3fe567995f3704
MariaKhmel <mariakhmel8@gmail.com>
2022-06-12 01:56:52 +0300

    final

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,11 +1,14 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+console.log(galleryItems);
 
 const createGallery = document.querySelector('.gallery');
 
 const galleryMarkUp = createGalleryItems(galleryItems);
+let instance = "";
 
-createGallery.innerHTML = galleryMarkUp();
+
+createGallery.innerHTML = galleryMarkUp;
 
 
 
@@ -18,14 +21,43 @@ function createGalleryItems() {
  <div class="gallery__item">
   <a class="gallery__link" href="${original}">
     <img
-      class="gallery__image "
+      class="gallery__image  "
       src="${preview}"
       data-source='${original}'
       alt="${description}"
     />
   </a>
 </div>`
-`) .join('')
+) .join('')
     }  ;
 
+function onImageClick(event) {
+    event.preventDefault();
+    const isPictureClicked = event.target.classList.contains('.gallery__image');
 
+    if (!isPictureClicked) {
+        return;
+    }
+
+  else {
+   instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600">
+`);
+    instance.show()
+  };
+
+  window.addEventListener('keydown', onEscKeyPress);
+}
+
+
+
+function onCloseModal() {
+  window.removeEventListener('keydown', onEscKeyPress);
+instance.close()
+}
+
+function onEscKeyPress(event) {
+  if (event.code === 'Escape') {
+    onCloseModal()
+ }
+}
